950302793c2cbef6b8864305badb82c3a52e9c3c
xiexc <xiexc@authine.com>
2020-10-24 11:34:17 +0800

    2

--- README.md
+++ README.md
@@ -1,12 +1,35 @@
-# check-cycle-complexity
+# cli工具——concc
 
-### useage
+#### 安装
 ```
-snpm i -g @souche/check-cycle-complexity
+snpm i -g check-cycle-complexity
 
-cd /yourcheckPath
+```
++ `version` 产看concc版本号
++  `sr` 代码圈复杂度统计（show result简写）
 
+#### version
+```
+concc version //显示concc版本
+```
+
+#### sr
+> 默认统计当前cmd目录
+```
+// 默认统计 圈复杂度>1
 concc sr
-// --min=? 过滤圈复杂度
+// --min 过滤圈复杂度
+concc sr --min=10 
 ```
-> note: 仅支持js文件，默认过滤当前目录下的node_moudles dist build output common_build文件夹
+### 结果展示
+
+![markdown](http://assets.ytuj.cn/img/Snipaste_2019-12-16_13-59-46.png)
+
+------------
+
+![avatar](http://assets.ytuj.cn/img/Snipaste_2019-12-16_14-00-01.png)
+
+
+
+
+> note: 仅支持 js，vue，jsx 文件，默认过滤当前目录下的 node_moudles dist build output common_build 文件夹
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,23 @@
 const cc = require("../index")
+const chalk = require("chalk")
+const { table } = require("table")
+const { Spinner } = require("cli-spinner")
+
+const M_TABLE_HEAD = ["类目", "数据"]
+const TABLE_HEAD = ["函数名", "函数类型", "复杂度", "文件名", "位置", "重构建议"]
+
+let spinner = new Spinner("")
+
+const loading = (title = "加载中...") => {
+    spinner.setSpinnerTitle(` 💫  ${title}  %s`)
+    spinner.setSpinnerString("⣾⣽⣻⢿⡿⣟⣯⣷")
+    spinner.start()
+}
+
+const stop = () => {
+    spinner.stop()
+    console.log()
+}
 
 function fmtNumber(v) {
     let n = parseFloat(v)
@@ -8,16 +27,38 @@ function fmtNumber(v) {
         return `${(n * 100).toFixed(2)}%`
     }
 }
+
 module.exports = async function(param) {
+    loading("正在执行代码复杂度检测...")
+
+    const start = Date.now()
+
     const ccResult = await cc(param)
+    stop()
+    console.log(`检测完成,耗费${Date.now() - start}ms`)
     const { normalRatio, slightRatio, seriousRatio, fileCount, funcCount, result, score } = ccResult
-    console.table({
-        "<5": fmtNumber(normalRatio),
-        "5~10": fmtNumber(slightRatio),
-        ">10": fmtNumber(seriousRatio),
-        fileCount,
-        funcCount,
-        score
-    })
-    console.table(result)
+
+    let mResultTable = [
+        ["1 < 圈复杂度 <= 5", chalk.green(fmtNumber(normalRatio))],
+        ["5 < 圈复杂度 <= 10", chalk.green(fmtNumber(slightRatio))],
+        ["10 < 圈复杂度", chalk.green(fmtNumber(seriousRatio))],
+        ["文件数量", chalk.green(fileCount)],
+        ["函数数量", chalk.green(funcCount)]
+    ]
+
+    mResultTable.unshift(M_TABLE_HEAD)
+
+    let resultTable = result.map(e => [
+        chalk.green(e.funcType),
+        chalk.green(e.funcName),
+        chalk.yellow(e.complexity),
+        chalk.green(e.fileName),
+        chalk.green(e.position),
+        chalk.green(e.advice)
+    ])
+
+    resultTable.unshift(TABLE_HEAD)
+
+    console.log(table(mResultTable))
+    console.log(table(resultTable))
 }
